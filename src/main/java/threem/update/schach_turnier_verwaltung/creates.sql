747cac61bfd07682ca3beb2e269e9237b1165779
samue <samuel.bauer0401@outlook.de>
2025-05-20 12:45:28 +0200

    first

--- src/main/java/threem/update/schach_turnier_verwaltung/creates.sql
+++ src/main/java/threem/update/schach_turnier_verwaltung/creates.sql
@@ -6,7 +6,6 @@ CREATE SEQUENCE tournaments_id_seq
     START WITH 1
     INCREMENT BY 1;
 
-
 Create Table tournaments (tournamentId numeric(5) primary key, name varchar(30), start_time timestamp, end_time timestamp)
 
 Create Table persons_tournaments (personId numeric(5) REFERENCES persons(PERSONID), tournamentId numeric(4) REFERENCES tournaments(TOURNAMENTID))
