9e3c151dcfc716414f57f674bde433cf4bb37d9a
samue <samuel.bauer0401@outlook.de>
2025-05-21 09:16:05 +0200

    lkdsafjlsafölasljdaölkgjdsaf

--- src/main/java/threem/update/schach_turnier_verwaltung/creates.sql
+++ src/main/java/threem/update/schach_turnier_verwaltung/creates.sql
@@ -1,17 +1,9 @@
-Create SEQUENCE person_id_seq
-    START WITH 1
-    INCREMENT BY 1;
-
-CREATE SEQUENCE tournaments_id_seq
-    START WITH 1
-    INCREMENT BY 1;
---hallo
 
 CREATE TABLE persons (personId numeric(5) GENERATED ALWAYS AS IDENTITY PRIMARY KEY, username varchar(20), password varchar(20), admin boolean, wins numeric(4), losses numeric(4), draws numeric(4))
 
-Create Table tournaments (tournamentId numeric(5) primary key, name varchar(30), start_time timestamp, end_time timestamp)
+Create Table tournaments (tournamentId numeric(5) GENERATED ALWAYS AS IDENTITY primary key, name varchar(30), start_time timestamp, end_time timestamp)
 
-Create Table persons_tournaments (personId numeric(5) REFERENCES persons(PERSONID), tournamentId numeric(4) REFERENCES tournaments(TOURNAMENTID))
+Create Table persons_tournaments (personId INT REFERENCES persons(PERSONID), tournamentId numeric(4) REFERENCES tournaments(TOURNAMENTID))
 
 DROP TABLE persons_tournaments;
 DROP TABLE persons;
