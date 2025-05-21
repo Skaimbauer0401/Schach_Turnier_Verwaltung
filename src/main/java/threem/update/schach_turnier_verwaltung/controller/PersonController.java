a164e779d4869e67a357d37f09dc9e2cfb6b8713
Skaimbauer <samuel.bauer0401@outlook.de>
2025-05-21 22:08:13 +0200

    ztr

--- src/main/java/threem/update/schach_turnier_verwaltung/controller/PersonController.java
+++ src/main/java/threem/update/schach_turnier_verwaltung/controller/PersonController.java
@@ -3,6 +3,7 @@ package threem.update.schach_turnier_verwaltung.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import threem.update.schach_turnier_verwaltung.data.Person;
 
 import java.sql.*;
 
@@ -14,15 +15,18 @@ public class PersonController {
     private String dbpassword = "DBAdmin";
 
     @GetMapping("/persons/person/{username}/{password}")
-    public String getPerson(@PathVariable String username, @PathVariable String password){
+    public String getPerson(@PathVariable String username, @PathVariable String password) {
         try {
             Connection con = DriverManager.getConnection(url,user,dbpassword);
-            PreparedStatement pstmt = con.prepareStatement("select * from person where username=? and password=?");
+            PreparedStatement pstmt = con.prepareStatement("select * from persons WHERE username = ? AND password =?");
             pstmt.setString(1,username);
             pstmt.setString(2,password);
             ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            Person temp = new Person();
+
+
             con.close();
-            System.out.println(rs.getInt(1)+rs.getString(2)+rs.getString(3)+rs.getBoolean(4)+rs.getInt(5)+rs.getInt(6)+rs.getInt(7));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
