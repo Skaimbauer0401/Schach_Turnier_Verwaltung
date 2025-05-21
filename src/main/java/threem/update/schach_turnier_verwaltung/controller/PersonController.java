1f6c30e4c31b818e112f2f9d997256688d2197ec
Skaimbauer <samuel.bauer0401@outlook.de>
2025-05-21 22:20:07 +0200

    ztr

--- src/main/java/threem/update/schach_turnier_verwaltung/controller/PersonController.java
+++ src/main/java/threem/update/schach_turnier_verwaltung/controller/PersonController.java
@@ -1,11 +1,12 @@
 package threem.update.schach_turnier_verwaltung.controller;
 
-import org.springframework.boot.json.GsonJsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import threem.update.schach_turnier_verwaltung.data.Person;
 
+import java.io.IOException;
 import java.sql.*;
 
 @RestController
@@ -25,13 +26,19 @@ public class PersonController {
             ResultSet rs = pstmt.executeQuery();
             rs.next();
             Person temp = new Person(rs.getInt("personId"),rs.getString("username"),rs.getString("password"),rs.getBoolean("admin"),rs.getInt("wins"),rs.getInt("losses"),rs.getInt("draws"));
-
-
+            rs.close();
             con.close();
+
+            // Convert Person object to JSON string
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonString = objectMapper.writeValueAsString(temp);
+
+            return jsonString;
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        return "tesmp";
     }
 }
