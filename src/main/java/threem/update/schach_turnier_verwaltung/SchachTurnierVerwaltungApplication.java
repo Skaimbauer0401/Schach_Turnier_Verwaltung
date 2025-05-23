9203a26051d90fc43ec02053080c2a272508109c
samue <samuel.bauer0401@outlook.de>
2025-05-23 13:15:05 +0200

    rewagsdhtgjfhdfjsghdgsf

--- src/main/java/threem/update/schach_turnier_verwaltung/SchachTurnierVerwaltungApplication.java
+++ src/main/java/threem/update/schach_turnier_verwaltung/SchachTurnierVerwaltungApplication.java
@@ -2,6 +2,9 @@ package threem.update.schach_turnier_verwaltung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SchachTurnierVerwaltungApplication {
@@ -10,4 +13,15 @@ public class SchachTurnierVerwaltungApplication {
         SpringApplication.run(SchachTurnierVerwaltungApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:63342")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
