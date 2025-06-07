b756528224bbcf40440d220a059f744246b5e731
Skaimbauer <samuel.bauer0401@outlook.de>
2025-06-07 13:38:52 +0200

    afgdfgadg

--- src/main/java/threem/update/schach_turnier_verwaltung/backend/services/TournamentService.java
+++ src/main/java/threem/update/schach_turnier_verwaltung/backend/services/TournamentService.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import threem.update.schach_turnier_verwaltung.backend.data.Person;
 import threem.update.schach_turnier_verwaltung.backend.data.Tournament;
 
